e4573f143f6f396f4785e1b25f93078cb2eabd4d
tejaswanipulugu7 <155130147+tejaswanipulugu7@users.noreply.github.com>
2023-12-30 11:09:22 +0530

    task 2

---
+++ weather app.py
@@ -0,0 +1,27 @@
+import requests
+
+def get_weather(api_key, city):
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "q": city,
+        "appid": api_key,
+        "units": "metric"  # You can change units to "imperial" for Fahrenheit
+    }
+
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        weather_data = response.json()
+        print(f"Weather in {city}:")
+        print(f"Temperature: {weather_data['main']['temp']}°C")
+        print(f"Description: {weather_data['weather'][0]['description']}")
+        print(f"Humidity: {weather_data['main']['humidity']}%")
+        print(f"Wind Speed: {weather_data['wind']['speed']} m/s")
+    else:
+        print("Failed to fetch weather data.")
+
+if __name__ == "__main__":
+    # Replace 'YOUR_API_KEY' with your actual OpenWeatherMap API key
+    api_key = "YOUR_API_KEY"
+    city_name = input("Enter city name: ")
+    get_weather(api_key, city_name)
